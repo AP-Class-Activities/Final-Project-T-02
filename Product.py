0027b61e72b143a4c15759aec43ba1da9ee14ac2
Morteza24 <mortezia4444.mz@gmail.com>
2021-06-01 16:21:25 +0430

    implemented product numbers

--- Product.py
+++ Product.py
@@ -1,4 +1,31 @@
+import json
+
+
 class Product:
-    def __inti__(self, name, explanation):
-        self.name = name
-        self.explanation = explanation
+    
+    def __init__(self, store, name, explanation):
+        self.__store = store
+        self.__load()
+        self.__name = name
+        self.__explanation = explanation
+        self.__product_number = self.gen_product_number()
+        self.__save()
+    
+    def __load(self):
+        with open(f"./DATABASE/{self.__store}/products.json", "rt") as products_json:
+            self.__products_json = json.load(products_json)
+    
+    def __save(self):
+        self.__products_json[self.__product_number] = [self.__name, self.__explanation]
+
+        with open(f"./DATABASE/{self.__store}/products.json", "wt") as products_json:
+            json.dump(self.__products_json, products_json)
+
+    def gen_product_number(self):
+        try:
+            last_product_number = int(list(self.__products_json.keys())[-1][2:])
+        except IndexError:
+            last_product_number = 0
+        
+        return "PR" + (6 - len(str(last_product_number+1))) * "0" + str(last_product_number+1)
+
