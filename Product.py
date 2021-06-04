76106e5026e92d7180651e9da47d3599f37636f0
Morteza24 <mortezia4444.mz@gmail.com>
2021-06-05 00:50:43 +0430

    product class major update

--- Product.py
+++ Product.py
@@ -1,43 +1,106 @@
 import json
 
 
-
 class Product:
 
-    def __init__(self, store, name, explanation, Price, property):
+    def __init__(self, store, name, explanation):
         self.__store = store
         self.__load()
         self.__name = name
         self.__explanation = explanation
-        self.__product_number = self.__gen_product_number()
+        self.__product_id = self.__gen_product_id()
+        self.__sellers_prices_stock = {}
+        self.__comments = []
         self.__save()
-        self.__Price = Price
-        self.__property = property
 
 
+    # -------------- Private Methods --------------
 
-    # to load products information from database 
-    def__load(self):
+    # to load products information from database
+    def __load(self):
         with open(f"./DATABASE/{self.__store}/products.json", "rt") as products_json:
             self.__products_json = json.load(products_json)
+        self.__name, self.__explanation, self.__sellers_prices_stock, self.__comments = self.__products_json[self.__product_id]
 
 
     # to save changes to database
-    def__save(self):
-        self.__products_json[self.__product_number] = [self.__name, self.__explanation]
+    def __save(self):
+        self.__products_json[self.__product_id] = [self.__name, self.__explanation, self.__sellers_prices_stock, self.__comments]
 
         with open(f"./DATABASE/{self.__store}/products.json", "wt") as products_json:
             json.dump(self.__products_json, products_json)
 
 
-    # to generate unique product number
-    def __gen_product_number(self):
+    # to generate unique product id
+    def __gen_product_id(self):
         try:
-            last_product_number = int(list(self.__products_json.keys())[-1][2:])
+            last_product_id = int(list(self.__products_json.keys())[-1][2:])
         except IndexError:
-            last_product_number = 0  # in case there are no products yet
+            last_product_id = 0  # in case there are no products yet
         
-        return "PR" + (6 - len(str(last_product_number+1))) * "0" + str(last_product_number+1)
+        return "PR" + (6 - len(str(last_product_id+1))) * "0" + str(last_product_id+1)
 
 
+    # -------------- Public Methods --------------
+    
+    # to increase/add-to product's stock
+    def add_stock(self, seller, price, stock):
+        self.__load()
+        self.__sellers_prices_stock[seller] = (price, stock)
+        self.__save()
+
+
+    # to add new comments
+    def add_comment(self, sender, comment):
+        self.__load()
+        self.__comments.append([sender, comment])
+        self.__save()
+
+
+    # -------------- Setters and Getters --------------
+
+    @property
+    def name(self):
+        self.__load()
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__load()
+        if not isinstance(value, str):
+            raise ValueError("name must be a string")
+        self.__name = value
+        self.__save()
+
+
+    @property
+    def explanation(self):
+        self.__load()
+        return self.__explanation
+
+    @explanation.setter
+    def explanation(self, value):
+        self.__load()
+        if not isinstance(value, str):
+            raise ValueError("explanation must be a string")
+        self.__explanation = value
+        self.__save()
+
+
+    @property
+    def price(self):
+        self.__load()
+        return min([i[0] for i in self.__sellers_prices_stock.values()])
+
+
+    @property
+    def stock(self):
+        self.__load()
+        return sum([i[1] for i in self.__sellers_prices_stock.values()])
+
+
+    @property
+    def comments(self):
+        self.__load()
+        return self.__comments
 
